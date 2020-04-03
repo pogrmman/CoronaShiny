7426a4b4722ad4aacd18807dcfae80f6cc6f9903
Alexander Mohn <pogrmman@gmail.com>
2020-04-03 11:51:30 -0500

    Fix FIPS codes for most counties to simplify pulling data from census bureau

--- dataCleaning.R
+++ dataCleaning.R
@@ -19,11 +19,27 @@ library(stringr)
 
 ### Functions ###
 cleanDataPostMar22 <- function(covidData) {
+  # Load in state FIPS codes
+  stateFIPS <- read.csv("Data/StateFIPS.csv", colClasses = c("FIPS" = "character"))
+  
   # Initial Cleaning
   covidData <- covidData %>%
     # Merge FIPS columns
     mutate(FIPS = as.character(FIPS), ï..FIPS = as.character(ï..FIPS)) %>%
     unite(FIPS, ï..FIPS, FIPS, na.rm=TRUE) %>%
+    # Make FIPS code include all 5 digits
+    mutate(FIPS = as.character(FIPS)) %>%
+    mutate(FIPS = ifelse(str_length(FIPS) != 5, paste("0", FIPS, sep = ""), FIPS)) %>%
+    # Separate FIPS into state and county
+    mutate(County_FIPS = substr(FIPS, 3, 5)) %>%
+    mutate(State_FIPS = substr(FIPS, 1, 2)) %>%
+    select(-FIPS) %>%
+    # Fix state FIPS codes
+    merge(stateFIPS, by.x = "Province_State", by.y = "State") %>%
+    mutate(State_FIPS = ifelse(State_FIPS != FIPS, FIPS, State_FIPS)) %>%
+    select(-FIPS) %>%
+    # Set unknown counties to NA
+    mutate(County_FIPS = ifelse(County_FIPS == "", NA, County_FIPS)) %>%
     # Separate out date and time
     separate(Last_Update, into=c("Date", "Time"), sep=" ")
   
@@ -58,7 +74,42 @@ cleanDataPostMar22 <- function(covidData) {
     filter(Province_State != "Recovered") %>%
     select(-Recovered) %>%
     # Filter out Wuhan Evacuee
-    filter(Province_State != "Wuhan Evacuee")
+    filter(Province_State != "Wuhan Evacuee") %>%
+    # Filter out territories (Census API has no info on them)
+    filter(County != "")
+  
+  # Fix missing FIPS codes
+  missing <- covidData %>% filter(is.na(County_FIPS)) %>%
+    mutate(County_FIPS = mapply(getFIPS, County, as.numeric(State_FIPS))) %>%
+    mutate(County_FIPS = as.character(County_FIPS)) %>%
+    mutate(County_FIPS = ifelse(str_length(County_FIPS) < 3,
+                                ifelse(str_length(County_FIPS) == 2,
+                                       paste("0", County_FIPS, sep = ""),
+                                       paste("00", County_FIPS, sep = "")),
+                                County_FIPS))
+  covidData <- covidData %>% filter(!is.na(County_FIPS)) %>%
+    bind_rows(missing)
   
   return(covidData)
 }
+
+# Find missing county FIPS codes
+getFIPS <- (function() {
+  lookupTbl <- read.csv("Data/all-geocodes-v2017.csv", encoding = "UTF-8") %>%
+    filter(X.U.FEFF.Summary.Level == 50)
+  return (function(name, stFIPS) {
+    # Select all counties in state
+    inState <- lookupTbl[lookupTbl$State.Code..FIPS. == stFIPS,]
+    names <- inState$Area.Name..including.legal.statistical.area.description.
+    # Fuzzy string match county name to state counties
+    idx <- agrep(name, names)
+    # Get county FIPS code(s)
+    result <- inState$County.Code..FIPS.[idx]
+    if(length(result) > 1) {
+      result <- result[1]
+    } else if(length(result) == 0) {
+      result <- NA
+    }
+    return(result)
+  })
+})()
